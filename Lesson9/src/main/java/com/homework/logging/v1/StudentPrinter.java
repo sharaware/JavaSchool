5718d00dbf3825577eda11fc22aa8f1c41e5eb07
sharaware <sharaware@inbox.ru>
2021-02-02 21:32:29 +0300

    Lesson9 Скопированы файлы из задания

---
+++ Lesson9/src/main/java/com/homework/logging/v1/StudentPrinter.java
@@ -0,0 +1,20 @@
+package com.homework.logging.v1;
+
+import com.homework.logging.Student;
+
+import java.util.List;
+
+public class StudentPrinter {
+
+    public void printAllNotPresentStudents(List<Student> students) {
+        //TODO Напечатать список всех студентов, которые отсутствовали на тесте (нет оценки) с уровнем warn
+    }
+
+    public void printAllStudents(List<Student> students) {
+        //TODO Напечатать список всех студентов с уровнем info
+    }
+
+    public void printAllStudentsWith2(List<Student> students) {
+        //TODO Напечатать список всех студентов, которые имеют оценку 2 c уровнем error
+    }
+}
---
+++ Lesson9/src/main/java/com/homework/logging/v2/NumberOfStudentsPrinter.java
@@ -0,0 +1,16 @@
+package com.homework.logging.v2;
+
+import com.homework.logging.Student;
+
+import java.util.List;
+
+public class NumberOfStudentsPrinter {
+    public void printNumberOfAllStudentsPresent(List<Student> students) {
+        //TODO Напечатать количество студентов, которые присутствовали на тесте (имеют оценку) с уровнем info
+    }
+
+    public void printNumberOfStudentsWith5And4(List<Student> students) {
+        //TODO Напечатать количество студентов, имеющих оценку 5, и имеющих оценку 4 (значения суммировать не надо). Записать необходимо в одну строку.
+        // Лог должен выводиться с уровнем debug
+    }
+}
