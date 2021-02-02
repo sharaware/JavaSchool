d591f1be94cf6d54ab45591c0186b264ba54528f
sharaware <sharaware@inbox.ru>
2021-02-03 02:24:10 +0300

    Lesson9 Решено логирование

--- Lesson9/src/main/java/com/homework/logging/v1/StudentPrinter.java
+++ Lesson9/src/main/java/com/homework/logging/v1/StudentPrinter.java
@@ -1,20 +1,36 @@
 package com.homework.logging.v1;
 
 import com.homework.logging.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class StudentPrinter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentPrinter.class);
 
     public void printAllNotPresentStudents(List<Student> students) {
-        //TODO Напечатать список всех студентов, которые отсутствовали на тесте (нет оценки) с уровнем warn
+        //Напечатать список всех студентов, которые отсутствовали на тесте (нет оценки) с уровнем warn
+        for(Student student : students) {
+            if (student.getResult() == null) {
+                LOGGER.warn("Студент {} {} гуляет", student.getFirstName(), student.getLastName());
+            }
+        }
     }
 
     public void printAllStudents(List<Student> students) {
-        //TODO Напечатать список всех студентов с уровнем info
+        //Напечатать список всех студентов с уровнем info
+        for(Student student : students) {
+            LOGGER.info(student.toString());
+        }
     }
 
     public void printAllStudentsWith2(List<Student> students) {
-        //TODO Напечатать список всех студентов, которые имеют оценку 2 c уровнем error
+        //Напечатать список всех студентов, которые имеют оценку 2 c уровнем error
+        for(Student student : students) {
+            if (student.getResult() != null && student.getResult() == 2) {
+                LOGGER.error("Студент {} {} получил двойку! На пересдачу!", student.getFirstName(), student.getLastName());
+            }
+        }
     }
 }
--- Lesson9/src/main/java/com/homework/logging/v2/NumberOfStudentsPrinter.java
+++ Lesson9/src/main/java/com/homework/logging/v2/NumberOfStudentsPrinter.java
@@ -1,16 +1,40 @@
 package com.homework.logging.v2;
 
 import com.homework.logging.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class NumberOfStudentsPrinter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NumberOfStudentsPrinter.class);
     public void printNumberOfAllStudentsPresent(List<Student> students) {
-        //TODO Напечатать количество студентов, которые присутствовали на тесте (имеют оценку) с уровнем info
+        //Напечатать количество студентов, которые присутствовали на тесте (имеют оценку) с уровнем info
+        int count = 0;
+        for(Student student : students) {
+            if(student.getResult() != null)
+                count++;
+        }
+        LOGGER.info("На тесте присутствовало {} студентов", count);
     }
 
     public void printNumberOfStudentsWith5And4(List<Student> students) {
-        //TODO Напечатать количество студентов, имеющих оценку 5, и имеющих оценку 4 (значения суммировать не надо). Записать необходимо в одну строку.
-        // Лог должен выводиться с уровнем debug
+        //Напечатать количество студентов, имеющих оценку 5, и имеющих оценку 4 (значения суммировать не надо). Записать необходимо в одну строку.
+        //Лог должен выводиться с уровнем debug
+        int cnt5 = 0, cnt4 = 0;
+        for(Student student : students) {
+            Integer result = student.getResult();
+            if (result != null) {
+                switch (result) {
+                    case 4:
+                        cnt4++;
+                        break;
+                    case 5:
+                        cnt5++;
+                        break;
+                }
+            }
+        }
+        LOGGER.debug("{} студентов получили оценку 5 и {} получили оценку 4", cnt5, cnt4);
     }
 }
